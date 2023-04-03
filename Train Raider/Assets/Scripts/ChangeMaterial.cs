93e6e71c746474c2f21b2a0a4250956f5f0aa6d1
JuicyOatmeal <felix.d.g.royle@gmail.com>
2023-04-03 18:28:22 +1200

    5th Session
    The follow script now "works" however it de-assigns itself when you press play for some reason. I'm trying to make the train car go transparent when you are inside it but so far no luck.

---
+++ Train Raider/Assets/Scripts/ChangeMaterial.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMaterial : MonoBehaviour
+{
+    public Material[] materials;
+    private int _currentIndex = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        public Renderer TargetRenderer;
+    }
+        
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+}
--- Train Raider/Assets/Scripts/FollowPlayer.cs
+++ Train Raider/Assets/Scripts/FollowPlayer.cs
@@ -4,18 +4,16 @@ using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
-    public GameObject camera;
     public GameObject player;
     // Start is called before the first frame update
     void Start()
     {
-        camera = GetComponent<GameObject>();
         player = GetComponent<GameObject>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        camera.transform.position = player.transform.position;
+        transform.position = player.transform.position;
     }
 }
