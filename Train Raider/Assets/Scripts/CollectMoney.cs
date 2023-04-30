7c0b4900fc79ffc7274bd085d1be0a1013704eb7
JuicyOatmeal <felix.d.g.royle@gmail.com>
2023-05-01 10:25:19 +1200

    15th Session

--- Train Raider/Assets/Scripts/CollectMoney.cs
+++ Train Raider/Assets/Scripts/CollectMoney.cs
@@ -6,10 +6,15 @@ public class CollectMoney : MonoBehaviour
 {
     GameObject moneyPrefab;
     public GameManager gameManager;
+    public ParticleSystem passiveParticle;
     void Start()
     {
         gameManager = GameObject.FindObjectOfType<GameManager>();
         moneyPrefab = gameObject;
+    }
+    private void Update()
+    {
+        Instantiate(passiveParticle, transform.position, passiveParticle.transform.rotation);
     }
     private void OnTriggerEnter(Collider other)
     {
